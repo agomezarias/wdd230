fc79afd4886fbb3705a54f4558fd64f33752de98
agomezarias <alberto_gomez_arias@yahoo.com>
2023-09-19 22:10:07 -0500

    dev tool fix and js for date last modified

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,14 +1,2 @@
-const pattern = /last_modif\s*=\s*([^;]*)/;
-
-const lastModifiedSpan = document.getElementById('lastModifiedValue');
-
-if (
-  Date.parse(document.lastModified) >
-  (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
-) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
-    location.pathname
-  }`;
-} else {
-  lastModifiedSpan.textContent = new Date(document.lastModified).toLocaleString(); // Display the last modified date
-}
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = date;
