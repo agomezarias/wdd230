980e6fb31e2af54fd7e69b69d937e21d36cc1cc6
agomezarias <alberto_gomez_arias@yahoo.com>
2023-09-25 01:03:06 -0500

    Dark mode and hamburger menu

---
+++ scripts/darkMode.js
@@ -0,0 +1,19 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const body= document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("Mode 🌑")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+        body.style.background = "#000";
+        body.style.color = "#fff";
+		modeButton.textContent = "🌕 Mode";
+	} else {
+		main.style.background = "#fff";
+		main.style.color = "#000";
+        body.style.background = "#fff";
+        body.style.color = "#000";
+		modeButton.textContent = "Mode 🌑";
+	}
+});
